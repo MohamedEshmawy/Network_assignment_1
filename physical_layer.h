e6cc85de30bb67516a2dbe125be3964cad843089
MohamedAliRashad <m.rashadnow@gmail.com>
2019-11-23 19:09:33 +0200

    Changes in Physical Layer

--- physical_layer.h
+++ physical_layer.h
@@ -7,33 +7,33 @@
 
 #include <queue>
 
-using namespace std;
-
 class Physical_Layer
 {
 public:
 	/*
 			sends data to the other side
 		*/
-	static void to_physical_layer(frame *f, int delay);
+	static void to_physical_layer(frame *f, int delay=0);
 
 	/*
 			returns the head of the received frames queue and pops the queue
 		*/
-	static void from_physical_layer(frame *r);
+	static frame* from_physical_layer();
 
 	/*
 			either creates or connects to a local tcp socket given its port 
 			and contains a while loop in which it listens to data from the other side
 			it pushes the data received in the recevied frames queue
 		*/
-	static void create_physical(bool isServer, int port);
+	static void physical(bool isServer, int port=8080);
 
 private:
 	/*
 		queue containing received frames
 	*/
 	static queue<frame> received_frames;
+	static queue<pair<frame, int>> sent_frames;
+
 };
 
 #endif
